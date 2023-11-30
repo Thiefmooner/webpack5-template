f428af61905c7bf3f4734381a630730a35ded0cd
Thiefmooner <moonthief@foxmail.com>
2023-11-30 09:29:42 +0800

    feature(global):add postcss-loader and plugin named poscss-preset-env

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -18,7 +18,28 @@ module.exports = (webpackEnv) => {
             rules:[
                 {
                     test:/.css$/i,
-                    use:["style-loader","css-loader"]
+                    use:[
+                        "style-loader",
+                        "css-loader",
+                        {
+                            loader: 'postcss-loader',  //css用来兼容浏览器的处理
+                            options: {
+                              postcssOptions: {
+                                plugins: [
+                                  [
+                                    'postcss-preset-env',  //postcss里，使用postcss-preset-env插件来自动添加前缀
+                                    {
+                                      autoprefixer: {
+                                        flexbox: 'no-2009',
+                                      },
+                                      stage: 3,
+                                    },
+                                  ],
+                                ],
+                              },
+                            }
+                        },
+                    ]
                 }
             ]
         },
